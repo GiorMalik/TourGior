666bfde3f0f580b9f7e96563d604a734a06e333e
Giorgino Malik <giorginomalik@gmail.com>
2025-10-26 15:42:37 +0000

    fix: Update Next.js config for new repository name

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 
 import type {NextConfig} from 'next';
 
+const repo = 'TourGior';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   typescript: {
     ignoreBuildErrors: true,
   },
